6fadf740d04379f22d5a5ba56df5c552a4539e6d
Karim Afas <karimafas@gmail.com>
2024-08-31 13:45:43 +0200

    chore: app route enum and router tidy up

---
+++ lib/core/router/app_route.dart
@@ -0,0 +1,6 @@
+enum AppRoute {
+  homeScreen('/');
+
+  const AppRoute(this.path);
+  final String path;
+}
--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,10 +1,11 @@
 import 'package:go_router/go_router.dart';
 import 'package:magic_workout_app/app/home/presentation/screens/home_screen.dart';
+import 'package:magic_workout_app/core/router/app_route.dart';
 
 final appRouter = GoRouter(
   routes: [
     GoRoute(
-      path: '/',
+      path: AppRoute.homeScreen.path,
       builder: (context, state) => const HomeScreen(),
     ),
   ],
